e373a7debd265f879d362e3ee775eaf3909c3d48
WidyaX1900 <tuderangga93@gmail,com>
2025-01-19 10:20:56 +0800

    Create application layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -8,10 +8,16 @@
     <title>Matimpal</title>
     @vite(['resources/css/app.css'])
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <script src="{{ config('app.asset_url') }}jquery/jquery-3.7.1.min.js"></script>
 </head>
 
 <body>
+    @yield('navbar')
+    <main class="container">
+        @yield('content')
+    </main>
+    @yield('footer')
     @vite(['resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
