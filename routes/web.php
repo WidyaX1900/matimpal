11f40f767e9dbddef33ec068630ae07ecda0a7b7
WidyaX1900 <tuderangga93@gmail,com>
2025-01-19 15:55:33 +0800

    Breeze components

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('chats.index');
+    return view('welcome');
 });
 
-Route::get('/chat/show', function () {
-    return view('chats.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/chat/friends', function () {
-    return view('chats.friends');
-});
+require __DIR__.'/auth.php';
