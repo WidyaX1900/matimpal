edabcc1a3e653fa396e08dd6500e30ff2129d1f7
WidyaX1900 <tuderangga93@gmail,com>
2025-01-19 21:51:41 +0800

    Group inner route inside auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,18 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('chats.index');
-})->middleware(['auth', 'verified'])->name('home');
-
-Route::get('/chat/show', function () {
-    return view('chats.show');
-});
-
-Route::get('/chat/friends', function () {
-    return view('chats.friends');
-});
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/', function () {
+        return view('chats.index');
+    })->middleware(['auth', 'verified'])->name('home');
+
+    Route::get('/chat/show', function () {
+        return view('chats.show');
+    })->name('chat.show');
+
+    Route::get('/chat/friends', function () {
+        return view('chats.friends');
+    })->name('chat.friends');
 });
 
 require __DIR__.'/auth.php';
