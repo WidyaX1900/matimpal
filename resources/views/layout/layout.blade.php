a57f2f8a2dbbf9b53858904aac54263a33552438
WidyaX1900 <tuderangga93@gmail,com>
2025-01-19 18:09:03 +0800

    Move custom css to style.css file

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Matimpal</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/css/style.css'])
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <script src="{{ config('app.asset_url') }}jquery/jquery-3.7.1.min.js"></script>
