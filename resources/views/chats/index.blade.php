027b55c6cfacd5c55079c59d26f44e504c1f06c9
WidyaX1900 <tuderangga93@gmail,com>
2025-01-19 11:28:23 +0800

    Make UI for chat page

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layout.layout')
 @extends('layout.navbar')
+@section('title', 'Chat')
 @section('content')
+    <section class="chat-content">
+        <ul class="p-0">
+            @for ($i = 0; $i < 10; $i++)
+                <li class="mb-3">
+                    <i class="fa-solid fa-circle-user"></i>
+                    <div class="chat-info">
+                        <div class="friend-info">
+                            <strong>Friend Name</strong>
+                            <small>Last message</small>
+                        </div>
+                        <small>19/01/2025</small>
+                    </div>
+                </li>
+            @endfor
+        </ul>
+    </section>
 @endsection
 @extends('layout.footer')
