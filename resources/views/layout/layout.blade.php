09a1ca359bdf36be124bf4f57b64e5eb829b2a6e
WidyaX1900 <tuderangga93@gmail,com>
2025-01-26 10:40:46 +0800

    AJAX configuration for video call

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Matimpal</title>
     @vite(['resources/css/app.css', 'resources/css/style.css'])
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FriendController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ServerController;
+use App\Http\Controllers\VideoCallController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
@@ -19,6 +20,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/server/test', [ServerController::class, 'test'])->name('server.tes');
     
     Route::get('/server/receive', [ServerController::class, 'receive'])->name('server.receive');
+    
+    Route::post('/videocall/startcall', [VideoCallController::class, 'startCall'])->name('videocall.startcall');
+    
+    Route::post('/videocall/closecall', [VideoCallController::class, 'closeCall'])->name('videocall.closecall');
+    
+    Route::post('/videocall/acceptcall', [VideoCallController::class, 'acceptCall'])->name('videocall.acceptcall');
+    
+    Route::post('/videocall/rejectcall', [VideoCallController::class, 'rejectCall'])->name('videocall.rejectcall');
 });
 
 require __DIR__.'/auth.php';
