70c9494542676663219d2dabbe8b6cced2a74878
WidyaX1900 <tuderangga93@gmail,com>
2025-01-26 21:07:05 +0800

    Create videocall connection

--- resources/js/videocall_room.js
+++ resources/js/videocall_room.js
@@ -1,29 +1,57 @@
-if(document.getElementById("vicall-room")) {
-    let localStream;
+import { Peer } from "https://esm.sh/peerjs@1.5.4?bundle-deps";
+
+if (document.getElementById("vicall-room")) {
     const localVideo = document.querySelector(".vicall-room .local-user video");
     const socket = io.connect("http://127.0.0.1:3000/");
     const room = $("#vicall-room").data("room");
-    const user = $(".vicall-room .local-user").data("local"); 
-    
-    socket.emit("join-videocall", { username: user, userId: 10, room });        
-    openCamera(localVideo, "user");
-    function openCamera(video, mode) {
+    const user = $(".vicall-room .local-user").data("local");
+    const peer = new Peer({
+        host: "127.0.0.1",
+        port: 3001,
+        path: "/"
+    });
+    let localStream;
+
+    peer.on("open", async (peerId) => {
+        socket.emit("join-videocall", { username: user, userId: peerId, room });
+        localStream = await getStream("user");
+        openCamera(localVideo, localStream);        
+    });
+
+    peer.on("call", (call) => {
+        call.answer(localStream);
+        call.on("stream", (remoteStream) => {
+            const remoteVideo = document.querySelector(
+                ".vicall-room .remote-user video"
+            );
+            openCamera(remoteVideo, remoteStream);            
+        });
+    });
+
+    async function getStream(mode) {
+        let camStream = navigator.mediaDevices.getUserMedia({
+            video: mode,
+            audio: true,
+        });
+        return camStream;
+    }
+
+    function openCamera(video, stream) {
+        video.srcObject = stream;
         video.onloadedmetadata = () => video.play();
-        navigator.mediaDevices
-            .getUserMedia({ video: mode, audio: true })
-            .then((stream) => {
-                localStream = stream;
-                video.srcObject = localStream;
-            })
-            .catch((error) => console.log(`Failed to open camera: ${error}`));
     }
 
     // Sockets
     socket.on("new-videocall", (data) => {
-        console.log(`User ${data.username} has joined video call with id ${data.userId} in ${data.room} room.`);        
-    });
-    socket.on("onair-videocall", (data) => {
-        console.log(data.videocall);        
+        setTimeout(() => {
+            const call = peer.call(data.userId, localStream);
+            call.on("stream", (remoteStream) => {
+                const remoteVideo = document.querySelector(
+                    ".vicall-room .remote-user video"
+                );
+                openCamera(remoteVideo, remoteStream);
+            });
+        }, 1000); 
     });
     // End Sockets
 }
--- resources/views/videocall/room.blade.php
+++ resources/views/videocall/room.blade.php
@@ -5,7 +5,15 @@
             <video muted playsinline></video>
         </div>
         <div class="remote-user">
-            <h1 class="text-light">{{ $vicall->secondary_user }}</h1>
+            <div class="user-info">
+                <i class="fa-solid fa-circle-user text-secondary"></i>
+                <h4 class="text-light mb-3">{{ $vicall->secondary_user }}</h4>
+                <div class="text-light d-flex align-items-center justify-content-center">
+                    <span class="spinner-grow spinner-grow-sm me-1"></span>
+                    <small>Waiting...</small>
+                </div>
+            </div>
+            <video playsinline></video>
         </div>
     </section>
 @endsection
