49b44b410fa56ab24ff68c68c9af4dc5e148f235
WidyaX1900 <tuderangga93@gmail,com>
2025-02-15 21:25:50 +0800

    Handle front-end if there's no video call's data

--- resources/views/videocall/index.blade.php
+++ resources/views/videocall/index.blade.php
@@ -8,38 +8,44 @@
 @endsection
 @section('content')
     <section class="videocall-content">
-        <ul class="p-0 friend-list">
-            @foreach ($videocalls as $list)
-                <li class="mb-3">
-                    <a>
-                        <i class="fa-solid fa-circle-user text-secondary"></i>
-                        <div class="vicall-info">
-                            <div class="friend-info">
-                                <strong>{{ $list->user->name }}</strong>
-                                <small class="text-secondary">
-                                    @if ($list->status === 'missed call' || $list->status === 'rejected')
-                                       <i class="fa-solid fa-video-slash text-danger"></i> 
-                                    @else
-                                       <i class="fa-solid fa-video text-success"></i> 
-                                    @endif
-
-                                    @if ($list->status === 'missed call' || $list->status === 'rejected')
-                                       {{ Str::ucfirst($list->status) }} 
-                                    @else
-                                        @if ($list->direction === 'outgoing')
-                                            Outgoing call
+        @if ($videocalls->isEmpty())
+            <div class="vh-100 d-flex justify-content-center align-items-center">
+                No Video Calls Available
+            </div>
+        @else
+            <ul class="p-0 friend-list">
+                @foreach ($videocalls as $list)
+                    <li class="mb-3">
+                        <a>
+                            <i class="fa-solid fa-circle-user text-secondary"></i>
+                            <div class="vicall-info">
+                                <div class="friend-info">
+                                    <strong>{{ $list->user->name }}</strong>
+                                    <small class="text-secondary">
+                                        @if ($list->status === 'missed call' || $list->status === 'rejected')
+                                        <i class="fa-solid fa-video-slash text-danger"></i> 
                                         @else
-                                            Incoming call
-                                        @endif 
-                                    @endif
-                                </small>
+                                        <i class="fa-solid fa-video text-success"></i> 
+                                        @endif
+
+                                        @if ($list->status === 'missed call' || $list->status === 'rejected')
+                                        {{ Str::ucfirst($list->status) }} 
+                                        @else
+                                            @if ($list->direction === 'outgoing')
+                                                Outgoing call
+                                            @else
+                                                Incoming call
+                                            @endif 
+                                        @endif
+                                    </small>
+                                </div>
                             </div>
-                        </div>
-                        <i class="fa-solid fa-video vicall-icon" data-receiver="{{ $list->user->name }}"></i>
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+                            <i class="fa-solid fa-video vicall-icon" data-receiver="{{ $list->user->name }}"></i>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>            
+        @endif
         <a href="/videocall/newcall" class="new-vicall-btn">
             <i class="fa-solid fa-video"></i>
         </a>    
